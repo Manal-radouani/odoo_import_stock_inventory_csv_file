1b37f4c31cfa131db82682ddf4c1ee86e636927d
yasr3mr <yasr3mr@gmail.com>
2019-08-24 16:43:55 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Import',
+	'summary': 'inventory',
+	'author': 'Notion Team by Yasser Omar',
+	'website': 'http://www.notionbiz.in',
+	'category': 'Import',
+	'version': '11.0.0.2',
+	'depends': ['base','stock'],
+	'data': [
+		'wizard/view_import_inventory.xml',
+		],
+
+	'installable': True,
+    'application': True,
+    'qweb': [
+    		],
+    "images":['static/description/Banner.png']
+}
+
